ab309806359546abe85e78532c6f52358be6d79f
rotimijoseph <rotimi.joseph@hotmail.com>
2026-05-01 22:46:06 +0100

    feat: add layout for testimonials

---
+++ football-coaching/src/components/Testimonials.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+export default function Testimonials() {
+    return (
+    <>
+        <h1>Player Testimonials</h1>
+        <p>Hear from international players who've elevated their careers with our coaching</p>
+        <ul>
+            <li>
+                Player 1
+            </li>
+            <li>
+                Player 2
+            </li>
+            <li>
+                Player 3
+            </li>
+        </ul>
+    </>
+    )
+}
