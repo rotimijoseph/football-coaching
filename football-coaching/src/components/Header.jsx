af554601a6277b73f10bc199a228db322cf42ed9
rotimijoseph <rotimi.joseph@hotmail.com>
2026-05-02 00:46:36 +0100

    update: styling on nav bar, add sticky navigation

--- football-coaching/src/components/Header.jsx
+++ football-coaching/src/components/Header.jsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export default function Header() {
     return (
         <nav id="nav">
-            <h1>Elite Football Coaching</h1>
+            <h2 >Elite Football Coaching</h2>
                 <ul>
                     <li><a href="#training-programs">Training Programs</a></li>
                     <li><a href="#testimonials">Testimonials</a></li>
