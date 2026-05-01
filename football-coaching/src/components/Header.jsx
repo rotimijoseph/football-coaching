677d5cf770182c28217e0bbac659ac14bb4ec19f
rotimijoseph <rotimi.joseph@hotmail.com>
2026-05-02 00:49:04 +0100

    update: nav styling, float right and left

--- football-coaching/src/components/Header.jsx
+++ football-coaching/src/components/Header.jsx
@@ -5,10 +5,10 @@ export default function Header() {
         <nav id="nav">
             <h2 >Elite Football Coaching</h2>
                 <ul>
-                    <li><a href="#training-programs">Training Programs</a></li>
-                    <li><a href="#testimonials">Testimonials</a></li>
-                    <li><a href="#contact">Contact</a></li>
                     <li><a>Book Free Session</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#testimonials">Testimonials</a></li>
+                    <li><a href="#training-programs">Training Programs</a></li>
                 </ul>
         </nav>
     )
