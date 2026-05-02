b945bc9f20725b4ec5c98ab6ea1916235b98c787
rotimijoseph <rotimi.joseph@hotmail.com>
2026-05-02 01:04:38 +0100

    update: make page full screen, style introduction buttons

--- football-coaching/src/components/Introduction.jsx
+++ football-coaching/src/components/Introduction.jsx
@@ -5,10 +5,10 @@ export default function Introduction() {
         <>
         <h1>Elevate Your Game to a Profressional Level</h1>
         <p>Transform your football skills with personalised coaching from experienced professionals. Join hundreds of players who've reached their potential.</p>
-        <button>
+        <button class="intro-button">
             Book Your Free Session
         </button>
-        <button>
+        <button class="intro-button">
             View Courses
         </button>
         </>
