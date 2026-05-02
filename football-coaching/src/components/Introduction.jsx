0c0a884e01fff0efce1997e96dfe411be12492ad
rotimijoseph <rotimi.joseph@hotmail.com>
2026-05-02 17:16:54 +0100

    update: styling for the introduction section

--- football-coaching/src/components/Introduction.jsx
+++ football-coaching/src/components/Introduction.jsx
@@ -3,14 +3,20 @@ import * as React from 'react'
 export default function Introduction() {
     return (
         <div class="intro">
-            <h1>Elevate Your Game to a Profressional Level</h1>
-            <p>Transform your football skills with personalised coaching from experienced professionals. Join hundreds of players who've reached their potential.</p>
-            <button class="intro-button">
-                Book Your Free Session
-            </button>
-            <button class="intro-button">
-                View Courses
-            </button>
+            <h1>Elevate Your Game <br/><br/><br/><br/> to a Profressional Level</h1>
+                <br/>
+            <p>Transform your football skills with personalised coaching from experienced professionals.
+                <br/>
+            Join hundreds of players who've reached their potential.</p>
+                <br/>
+            <div class="intro-button-group">
+                <button class="intro-button">
+                    Book Your Free Session
+                </button>
+                <button class="intro-button">
+                    View Courses
+                </button>
+            </div>
         </div>
     )
 }
