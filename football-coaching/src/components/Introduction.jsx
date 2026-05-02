529d132bf6d0db1c86754735028786b25c5318f4
rotimijoseph <rotimi.joseph@hotmail.com>
2026-05-02 01:40:34 +0100

    update: add background image to assets and load on screen

--- football-coaching/src/components/Introduction.jsx
+++ football-coaching/src/components/Introduction.jsx
@@ -2,15 +2,15 @@ import * as React from 'react'
 
 export default function Introduction() {
     return (
-        <>
-        <h1>Elevate Your Game to a Profressional Level</h1>
-        <p>Transform your football skills with personalised coaching from experienced professionals. Join hundreds of players who've reached their potential.</p>
-        <button class="intro-button">
-            Book Your Free Session
-        </button>
-        <button class="intro-button">
-            View Courses
-        </button>
-        </>
+        <div class="intro">
+            <h1>Elevate Your Game to a Profressional Level</h1>
+            <p>Transform your football skills with personalised coaching from experienced professionals. Join hundreds of players who've reached their potential.</p>
+            <button class="intro-button">
+                Book Your Free Session
+            </button>
+            <button class="intro-button">
+                View Courses
+            </button>
+        </div>
     )
 }
